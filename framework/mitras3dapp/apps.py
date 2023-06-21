54ca0b7c5c12149de267c88c7f03ef3b5e654051
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-06-21 14:14:43 +0530

    Rollback to Dynamic site

--- framework/mitras3dapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-#python manage.py runserver
-class Mitras3DappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mitras3dapp'
