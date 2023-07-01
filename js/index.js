f73c48f2e4db3154a96b63edba838b2be01c4398
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-07-02 01:50:39 +0530

    changes for 900px size

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 
 
+// NAVIGATION OPEN
+
+const butMenuEl = document.querySelector(".nav-menu-but");
+const NavEl = document.querySelector("nav .nav-div");
+
+butMenuEl.addEventListener("click", function(){
+    NavEl.classList.toggle("menu-open");
+});
