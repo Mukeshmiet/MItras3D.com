31c38b1eb836ebc13229684155ae563db16c6316
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-07-23 03:54:02 +0530

    changes for blog page

--- js/index.js
+++ js/index.js
@@ -16,3 +16,15 @@ const yearEl = document.querySelector("#copyright span");
 const currentYear=new Date().getFullYear();
 
 yearEl.textContent = currentYear;
+
+
+// Show the loader
+document.querySelector('.loader-container').style.opacity = 1;
+document.querySelector('.loader-container').style.pointerEvents = 'all';
+
+// Hide the loader after 5 seconds (or any desired duration)
+setTimeout(() => {
+    document.querySelector('.loader-container').style.opacity = 0;
+    document.querySelector('.loader-container').style.pointerEvents = 'none';
+}, 3000); // 5000 milliseconds (5 seconds) in this example
+
